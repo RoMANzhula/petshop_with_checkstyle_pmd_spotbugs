241d6337708474d3c9403bdae9788bde192bed7a
RoMANzhula <roman1985al@ukr.net>
2025-05-19 23:30:42 +0300

    Add PetshopWithCheckstylePmdSpotbugsApplication

---
+++ src/main/java/org/romanzhula/petshop_with_checkstyle_pmd_spotbugs/PetshopWithCheckstylePmdSpotbugsApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.petshop_with_checkstyle_pmd_spotbugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopWithCheckstylePmdSpotbugsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopWithCheckstylePmdSpotbugsApplication.class, args);
+	}
+
+}
