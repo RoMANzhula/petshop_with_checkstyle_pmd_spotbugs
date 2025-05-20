a317e3dd209c5b0bc90daa3401ce21ba8883cffe
RoMANzhula <roman1985al@ukr.net>
2025-05-20 21:45:18 +0300

    Add AnimalServiceImpl - getAllAnimals method

---
+++ src/main/java/org/romanzhula/petshop_with_checkstyle_pmd_spotbugs/services/impls/AnimalServiceImpl.java
@@ -0,0 +1,25 @@
+package org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.services.impls;
+
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.models.Animal;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.repositories.AnimalRepository;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.services.AnimalService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class AnimalServiceImpl implements AnimalService {
+
+    private final AnimalRepository animalRepository;
+
+    public AnimalServiceImpl(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    @Override
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+}
