460d540a7f0e965ccd3f2593884db79a30a4fda9
RoMANzhula <roman1985al@ukr.net>
2025-05-20 21:45:02 +0300

    Add AnimalService as interface - getAllAnimals method

---
+++ src/main/java/org/romanzhula/petshop_with_checkstyle_pmd_spotbugs/services/AnimalService.java
@@ -0,0 +1,12 @@
+package org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.services;
+
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.models.Animal;
+
+import java.util.List;
+
+
+public interface AnimalService {
+
+    List<Animal> getAllAnimals();
+
+}
