dcd38b88e887e01cfcf065c2684b5127b3aca1fc
RoMANzhula <roman1985al@ukr.net>
2025-05-20 21:44:19 +0300

    Add AnimalController - getAllAnimals method

---
+++ src/main/java/org/romanzhula/petshop_with_checkstyle_pmd_spotbugs/controllers/AnimalController.java
@@ -0,0 +1,27 @@
+package org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.controllers;
+
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.models.Animal;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.services.AnimalService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/animals")
+public class AnimalController {
+
+    private final AnimalService animalService;
+
+    public AnimalController(AnimalService animalService) {
+        this.animalService = animalService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Animal>> getAllAnimals() {
+        return ResponseEntity.ok(animalService.getAllAnimals());
+    }
+
+}
