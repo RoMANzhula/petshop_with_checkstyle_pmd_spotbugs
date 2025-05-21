cad6cae5f7f394924c58b4b6aab467cbc0aed547
RoMANzhula <roman1985al@ukr.net>
2025-05-21 22:22:41 +0300

    Add AnimalMapper

---
+++ src/main/java/org/romanzhula/petshop_with_checkstyle_pmd_spotbugs/mappers/AnimalMapper.java
@@ -0,0 +1,32 @@
+package org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.dto.request.AnimalRequest;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.dto.response.AnimalResponse;
+import org.romanzhula.petshop_with_checkstyle_pmd_spotbugs.models.Animal;
+
+import java.util.List;
+
+
+@Mapper
+public interface AnimalMapper {
+
+    AnimalMapper INSTANCE = Mappers.getMapper(AnimalMapper.class);
+
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "species", target = "species")
+    AnimalResponse toResponse(Animal animal);
+
+
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "species", target = "species")
+    Animal toEntity(AnimalRequest animalRequest);
+
+    
+    List<AnimalResponse> toResponseList(List<Animal> animals);
+
+}
